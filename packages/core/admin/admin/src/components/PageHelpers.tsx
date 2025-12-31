3f5b86b14dabfd9a27ac5633ab5cb03f8d35a547
Waf <waf.freelancer@gmail.com>
2025-12-31 11:16:07 +0700

    Update document title format in Title component
    Remove ' | Strapi' suffix from document title.

--- packages/core/admin/admin/src/components/PageHelpers.tsx
+++ packages/core/admin/admin/src/components/PageHelpers.tsx
@@ -255,7 +255,7 @@ export interface TitleProps {
  */
 const Title = ({ children: title }: TitleProps) => {
   React.useEffect(() => {
-    document.title = `${title} | Strapi`;
+    document.title = `${title}`;
   }, [title]);
 
   return null;
